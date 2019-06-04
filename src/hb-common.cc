17449d5527bc9c8644f4793a681b289b3d601e1d
mortentor <morten.tor@gmail.com>
2019-06-04 14:27:27 +0100

    fixed compilation issues

--- src/hb-common.cc
+++ src/hb-common.cc
@@ -67,7 +67,7 @@ _hb_options_init ()
         p = c + strlen (c);
 
 #define OPTION(name, symbol) \
-	if (0 == strncmp (c, name, p - c) && strlen (name) == p - c) do { u.opts.symbol = true; } while (0)
+	if (0 == strncmp (c, name, p - c) && strlen (name) == (size_t) ( p - c ) ) do { u.opts.symbol = true; } while (0)
 
       OPTION ("uniscribe-bug-compatible", uniscribe_bug_compatible);
       OPTION ("aat", aat);
